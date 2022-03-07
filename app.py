1db80dabeae523aeca98d90c0b521a232e953a07
Abdulrahman Abdullah <96795264+aabdullah014@users.noreply.github.com>
2022-03-07 10:38:53 -0700

    authorization

--- app.py
+++ app.py
@@ -1,19 +1,72 @@
 from flask import Flask, jsonify, request, render_template
+from flask_restful import Resource, Api, reqparse
+from flask_jwt import JWT, jwt_required
+from auth import authenticate, identity
 
 #give file unique name
 app = Flask(__name__)
+app.secret_key = 'secretysecret'
 
-members = [
-    {
-        'name': 'Abdulrahman',
-        'tasks': [
+api = Api(app)
+
+jwt = JWT(app, authenticate, identity)
+
+
+members = []
+
+tasks = [
             {
                 'name': 'practice Task',
                 'due_date': 2282022
             }
         ]
-    }
-]
+
+class Task(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('due_date',
+        type = int,
+        required = True,
+        help = "This field cannot be left blank"
+    )
+    data = parser.parse_args()
+
+    @jwt_required
+    def get(self, name):
+        task = next(filter(lambda x: x['name'] == name, tasks), None)
+
+        return {'task': task}, 200 if task else 404
+
+    @jwt_required
+    def post(self, name):
+        data = Task.parser.parse_args()
+
+        if next(filter(lambda x: x['name'] == name, tasks), None) is not None:
+            return {'message': 'This item already exists.'}, 400
+
+        task = {'name': name, 'due_date': data['due_date']}
+        tasks.append(task)
+
+    @jwt_required
+    def delete(self, name):
+        global tasks
+        tasks = list(filter(lambda x: x['name'] != name, tasks))
+        return {'message': 'Task deleted.'}
+
+    @jwt_required
+    def put(self, name):
+        data = Task.parser.parse_args()
+
+        task = next(filter(lambda x: x['name'] == name, tasks), None)
+        if not task:
+            task = {'name': name, 'due_date': data['due_date']}
+            tasks.append(task)
+        else:
+            task.update(data)
+        return task
+
+class TaskList(Resource):
+    def get(self):
+        return {'tasks': tasks}
 
 #render index.html file from templates folder
 @app.route('/')
@@ -71,5 +124,7 @@ def create_tasks_per_member(name):
     
     return jsonify({'message': 'member not found'})
 
+api.add_resource(Task, '/task/<string:name>')
+api.add_resource(TaskList, '/tasks')
 
 app.run(port=3000)
---
+++ auth.py
@@ -0,0 +1,19 @@
+from user import Member
+
+members = [
+    Member(1, 'bob', 'dingus')
+]
+
+username_mapping = {m.username: m for m in members}
+
+userid_mapping = {m.id: m for m in members}
+
+
+def authenticate(username, password):
+    user = username_mapping.get(username, None)
+    if user and user.password == password:
+        return user
+
+def identity(payload):
+    user_id = payload['identity']
+    return userid_mapping.get(user_id, None)
